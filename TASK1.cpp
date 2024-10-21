d505b89da3fe5d6972bb3ea6b25e31ac7d3b4c61
callmehaider <thehaiderplays@gmail.com>
2024-10-21 14:01:18 +0500

    task c++

---
+++ TASK1.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "*******************" << endl;
+	cout << "  Hello World " << endl;
+	cout << "*******************" << endl;
+
+	//task 2
+	cout << "************" << endl;
+	cout << "*          *" << endl;
+	cout << "***********" << endl;
+
+	//task3
+	cout << "\t    *" << endl;
+	cout << "\t*\t*" << endl;
+	cout << "     *\t\t    *" << endl;
+	cout << "    ******************" << endl;
+
+	//task4
+	cout << "   *" << endl;
+	cout << "  ***" << endl;
+	cout << " *****" << endl;
+	cout << "*******" << endl;
+	//task5
+	cout << " ****" << endl;
+	cout << "*    *" << endl;
+	cout << "*    *" << endl;
+	cout << " ****" << endl;
+	//task6
+	cout << "******" << endl;
+	cout << "*    *" << endl;
+	cout << "*    *" << endl;
+	cout << "******" << endl;
+	//task7
+	cout << "    *" << endl;
+	cout << "  *    *" << endl;
+	cout << "*\t *" << endl;
+	cout << "  *    *" << endl;
+	cout << "    *" << endl;
+	//task8
+	cout << "    *" << endl;
+	cout << " *******" << endl;
+	cout << "**********" << endl;
+	cout << " *******" << endl;
+	cout << "    *" << endl;
+	//task9
+	cout << "@@@@@@@@@@@@" << endl;
+	cout << "@  @    @  @" << endl;
+	cout << "@ @      @ @" << endl;
+	cout << "@@\t  @@" << endl;
+	cout << "@\t   @" << endl;
+	cout << "@@@@@@@@@@@@" << endl;
+	//task11
+	cout << "NAME:Haidered" << endl;
+	cout << "University ID : L1f23BSDS0012" << endl;
+	cout << "Father Name:Muhammad Ahmad" << endl;
+	cout << "Address: Block 1, Johartown, Lahore " << endl;
+	cout << "Semester :1" << endl;
+	cout << "Course : Introduction to Computer Lab " << endl;
+	return 0;
+
+}
